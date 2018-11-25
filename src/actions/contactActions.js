e1f3e97b14e85cef3f5c2aeda8e4259ff7668e58
hemant <nhemnt@gmail.com>
2018-11-25 15:27:47 +0530

    error handling on delete contact

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -9,11 +9,20 @@ export const getContacts = ()=> async dispatch => {
 }
 
 export const deleteContact = (id) => async dispatch => {
-    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
-    dispatch ({
-        type:DELETE_CONTACT,
-        payload: id
-    })
+    try{
+        await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+        dispatch ({
+            type:DELETE_CONTACT,
+            payload: id
+        })
+    }
+    catch(e){
+        dispatch ({
+            type:DELETE_CONTACT,
+            payload: id
+        })
+    }
+
 }
 
 export const addContact = (contact)=> async dispatch => {
